b028ed842282c4b83b940cea3cf7e97de9233564
Kiran Kumar Roy <kkroy22@gmail.com>
2020-03-31 19:33:33 +0530

    added

---
+++ nix/dotfile/bash/bash-man.sh
@@ -0,0 +1,7 @@
+# https://www.bikky.space
+# https://www.owcsx.science
+# https://www.linkedin.com/in/kkroy22/
+
+#echo "Updating configuration"
+#(cd $HOME/bundle/git/My-Sweet-Little-Sys && git pull && git submodule update --init --recursive)
+source $HOME/bundle/git/xMachine/nix/dotfile/env.sh
---
+++ nix/dotfile/env.sh
@@ -0,0 +1,12 @@
+# https://www.bikky.space
+# https://www.owcsx.science
+# https://www.linkedin.com/in/kkroy22/
+
+JAVA_HOME=$HOME/bundle/AppData/Jdk
+
+NODE=$HOME/bundle/AppData/Node
+FIREFOX=$HOME/bundle/AppData/firefox
+
+NGROK=$HOME/bundle/AppData/ngrok
+
+PATH=$PATH:$JAVA_HOME/bin:$NODE/bin:$FIREFOX/:$NGROK/
