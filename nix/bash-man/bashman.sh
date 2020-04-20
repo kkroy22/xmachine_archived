ddc6b57c43a86bceabb03b786cd984c40097150d
Kiran Kumar Roy <kkroy22@gmail.com>
2020-04-20 10:25:55 +0530

    updated

--- nix/bash-man/bashman.sh
+++ nix/bash-man/bashman.sh
@@ -1,10 +1,13 @@
-source $HOME/bundle/roaming/xmachine/nix/xenv.sh
 (cd $HOME/bundle/roaming/xmachine && git pull && git submodule update --init --recursive)
 
+source $HOME/bundle/roaming/xmachine/nix/xenv.sh
+
 DEFAULT=$P1
+# ROT='\e[30;101m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;101m>\e[0m'
+ROT='\e[30;41m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;41m>\e[0m'
 REG='\e[30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
 SUD='\e[30;43m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;43m>\e[0m'
-ROT='\e[30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
+
 
 if [ "$USER" = "root" ]; then
     PS1=$ROT
@@ -21,6 +24,6 @@ fi
 if command -v tmux>/dev/null; then
 	[ -z $TMUX ] && exec tmux
 else 
-	echo "tmux not installed. Run deploy to configure dependencies"
+	echo "Hello from the other side"
 fi
 
