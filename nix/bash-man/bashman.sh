103a76ebf01c307b9673b56e18156c0b9ccbb808
Kiran Kumar Roy <kkroy22@gmail.com>
2020-04-20 13:15:21 +0530

    rot

--- nix/bash-man/bashman.sh
+++ nix/bash-man/bashman.sh
@@ -1,13 +1,14 @@
-(cd $HOME/bundle/roaming/xmachine && git pull && git submodule update --init --recursive)
+if [ "$USER" != "compute" ]; then
+    (cd $HOME/bundle/roaming/xmachine && git pull && git submodule update --init --recursive)
+fi
+
+NIXPS1=$PS1
 
 source $HOME/bundle/roaming/xmachine/nix/xenv.sh
 
-DEFAULT=$P1
-# ROT='\e[30;101m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;101m>\e[0m'
-ROT='\e[30;41m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;41m>\e[0m'
-REG='\e[30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
-SUD='\e[30;43m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;43m>\e[0m'
-
+ROT='\e[0;30;41m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;41m>\e[0m'
+REG='\e[0;30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
+SUD='\e[0;30;43m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;43m>\e[0m'
 
 if [ "$USER" = "root" ]; then
     PS1=$ROT
@@ -16,10 +17,8 @@ elif [ "$USER" = "bikky" ]; then
 elif [ "$USER" = "compute" ]; then
     PS1=$REG
 else
-    PS1=$DEFAULT
+    PS1=$NIXPS1
 fi
-
-
 
 if command -v tmux>/dev/null; then
 	[ -z $TMUX ] && exec tmux
