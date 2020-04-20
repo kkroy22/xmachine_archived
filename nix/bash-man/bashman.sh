ed47d12f98f5362acbbeabb618302a3496d8af7f
Kiran Kumar Roy <kkroy22@gmail.com>
2020-04-20 10:11:55 +0530

    update

--- nix/bash-man/bashman.sh
+++ nix/bash-man/bashman.sh
@@ -1,1 +1,26 @@
 source $HOME/bundle/roaming/xmachine/nix/xenv.sh
+(cd $HOME/bundle/roaming/xmachine && git pull && git submodule update --init --recursive)
+
+DEFAULT=$P1
+REG='\e[30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
+SUD='\e[30;43m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;43m>\e[0m'
+ROT='\e[30;42m[\u]\e[0m-\e[30;104m[\w]\e[0m\n\e[30;42m>\e[0m'
+
+if [ "$USER" = "root" ]; then
+    PS1=$ROT
+elif [ "$USER" = "bikky" ]; then
+    PS1=$SUD
+elif [ "$USER" = "compute" ]; then
+    PS1=$REG
+else
+    PS1=$DEFAULT
+fi
+
+
+
+if command -v tmux>/dev/null; then
+	[ -z $TMUX ] && exec tmux
+else 
+	echo "tmux not installed. Run deploy to configure dependencies"
+fi
+
--- nix/xenv.sh
+++ nix/xenv.sh
@@ -1,5 +1,6 @@
 export LD_LIBRARY_PATH=$HOME/bundle/local/lib
 
+XSS=/home/compute/bundle/roaming/xmachine
 LOCALDIR=$HOME/bundle/local/bin
 
 PATH=$PATH:$LOCALDIR
