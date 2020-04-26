7e65ba8665241d6c700861d1aadc677f9eab4585
Kiran Kumar Roy <kkroy22@gmail.com>
2020-04-26 08:57:37 +0530

    off update

--- nix/bash-man/bashman.sh
+++ nix/bash-man/bashman.sh
@@ -1,4 +1,4 @@
-XUR="compute2"
+XUR="kkroy22"
 
 if [ "$USER" != $XUR ]; then
     (cd $HOME/bundle/roaming/xmachine && git pull && git submodule update --init --recursive)
@@ -12,19 +12,23 @@ source $HOME/bundle/roaming/xmachine/nix/xenv.sh
 ROTC='\e[0;30;41m'
 REGC='\e[0;30;42m'
 SUDC='\e[0;30;43m'
+OFFC='\e[0;97;100m'
 DIRC='\e[30;104m'
 ENDC='\e[0m'
 
 ROTPS1="\[$ROTC\][\u]\[$ENDC\]-\[$DIRC\][\w]\[$ENDC\]\n\[$ROTC\]>\[$ENDC\]"
 SUDPS1="\[$SUDC\][\u]\[$ENDC\]-\[$DIRC\][\w]\[$ENDC\]\n\[$SUDC\]>\[$ENDC\]"
 REGPS1="\[$REGC\][\u]\[$ENDC\]-\[$DIRC\][\w]\[$ENDC\]\n\[$REGC\]>\[$ENDC\]"
+OFFPS1="\[$OFFC\][\u]\[$ENDC\]-\[$DIRC\][\w]\[$ENDC\]\n\[$OFFC\]>\[$ENDC\]"
 
 if [ "$USER" = "root" ]; then
     PS1=$ROTPS1
 elif [ "$USER" = "bikky" ]; then
     PS1=$SUDPS1
-elif [ "$USER" = $XUR ]; then
+elif [ "$USER" = "compute" ]; then
     PS1=$REGPS1
+elif [ "$USER" = "kkroy22" ]; then
+    PS1=$OFFPS1
 else
     PS1=$NIXPS1
 fi
