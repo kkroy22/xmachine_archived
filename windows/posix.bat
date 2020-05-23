4666731e2927bc2f5b5c49201488de364e1b17d6
Kiran Kumar Roy <kkroy22@gmail.com>
2020-05-23 19:05:55 +0530

    posix

--- windows/posix.bat
+++ windows/posix.bat
@@ -1,5 +1,5 @@
 echo off
-set PATH=%PATH%;D:\posix\usr\bin
+set PATH=%PATH%;D:\posix\usr\bin;D:\posix\mingw64\bin
 echo Posix on Windows by www.owcsx.science
 echo Run bash for linux Shell
 "C:\WINDOWS\system32\cmd.exe"
